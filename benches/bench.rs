7d615830743f7a0375c863d5b26e814790f98a0f
hiratasa <36002166+hiratasa@users.noreply.github.com>
2023-03-27 23:18:48 +0900

    標準エラー出力に出力

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,7 +2,8 @@ use bencher::{benchmark_group, benchmark_main, Bencher};
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 use rand::Rng;
-use std::fmt::Write;
+use std::io;
+use std::io::Write;
 
 const N: usize = 100000;
 
@@ -12,81 +13,64 @@ static VALUES: Lazy<Vec<usize>> = Lazy::new(|| {
     (0..N).map(|_| rng.gen_range(0..1000000)).collect()
 });
 
-fn to_string_and_join(bench: &mut Bencher) {
-    let values = &*VALUES;
-    bench.iter(|| -> String {
-        values
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join(" ")
-    })
-}
-
-fn fold_by_to_string_without_capacity(bench: &mut Bencher) {
-    let values = &*VALUES;
-    bench.iter(|| -> String {
-        values
-            .iter()
-            .map(|s| s.to_string())
-            .fold(String::new(), |mut acc, cur| {
-                acc.push_str(&cur);
-                acc.push_str(" ");
-                acc
-            })
-    })
-}
-
-fn fold_by_to_string_with_capacity(bench: &mut Bencher) {
-    let values = &*VALUES;
-    bench.iter(|| -> String {
-        values
-            .iter()
-            .map(|s| s.to_string())
-            // 数値一つが最大6桁 + separator1文字で7文字
-            .fold(String::with_capacity(7 * values.len()), |mut acc, cur| {
-                acc.push_str(&cur);
-                acc.push_str(" ");
-                acc
-            })
-    })
-}
-
 fn itertools_join(bench: &mut Bencher) {
     let values = &*VALUES;
-    bench.iter(|| -> String { values.iter().join(" ") })
-}
-
-fn fold_by_write_without_capacity(bench: &mut Bencher) {
-    let values = &*VALUES;
-    bench.iter(|| -> String {
-        values.iter().fold(String::new(), |mut acc, cur| {
-            write!(&mut acc, "{} ", cur).unwrap();
-            acc
-        })
+    bench.iter(|| {
+        eprintln!("{}", values.iter().join(" "));
     })
 }
 
-fn fold_by_write_with_capacity(bench: &mut Bencher) {
+fn itertools_format(bench: &mut Bencher) {
     let values = &*VALUES;
-    bench.iter(|| -> String {
-        values
-            .iter()
-            // 数値一つが最大6桁 + separator1文字で7文字
-            .fold(String::with_capacity(7 * values.len()), |mut acc, cur| {
-                write!(&mut acc, "{} ", cur).unwrap();
-                acc
-            })
+    bench.iter(|| {
+        eprintln!("{}", values.iter().format(" "));
+    })
+}
+
+fn foreach_print(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        values.iter().for_each(|x| {
+            eprint!("{} ", x);
+        });
+        eprintln!();
+    })
+}
+
+fn itertools_join_with_bufwriter(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        let mut stderr = io::BufWriter::new(io::stderr().lock());
+        writeln!(stderr, "{}", values.iter().join(" ")).unwrap();
+    })
+}
+
+fn itertools_format_with_bufwriter(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        let mut stderr = io::BufWriter::new(io::stderr().lock());
+        writeln!(stderr, "{}", values.iter().format(" ")).unwrap();
+    })
+}
+
+fn foreach_print_with_bufwriter(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        let mut stderr = io::BufWriter::new(io::stderr().lock());
+        values.iter().for_each(|x| {
+            write!(stderr, "{} ", x).unwrap();
+        });
+        writeln!(stderr).unwrap();
     })
 }
 
 benchmark_group!(
     benches,
-    to_string_and_join,
-    fold_by_to_string_without_capacity,
-    fold_by_to_string_with_capacity,
     itertools_join,
-    fold_by_write_without_capacity,
-    fold_by_write_with_capacity
+    itertools_format,
+    foreach_print,
+    itertools_join_with_bufwriter,
+    itertools_format_with_bufwriter,
+    foreach_print_with_bufwriter,
 );
 benchmark_main!(benches);
